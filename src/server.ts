6cb4dcfad0affa2d566d01c3f7b26a998fe2347a
Takayoshi Uchida <takayoshi-uchida@mail.nissan.co.jp>
2020-01-30 17:16:09 +0900

    get request client ip address, add .cfignore

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,12 @@ import * as Express from 'express';
 require('dotenv').config();
 const app = Express();
 
+app.use((req: Express.Request, res: Express.Response, next: Express.NextFunction) => {
+    console.log(`req.connection.remoteAddress: ${req.connection.remoteAddress}`);
+    console.log(`req.headers['x-forwarded-for']: ${req.headers['x-forwarded-for']}`);
+    next();
+});
+
 app.get(
     '/',
     (req: Express.Request, res: Express.Response) => {
@@ -12,6 +18,7 @@ app.get(
 app.get(
     '/internal-api',
     (req: Express.Request, res: Express.Response) => {
+        console.log(`req.headers: ${JSON.stringify(req.headers)}`);
         return res.json({
             data: { from: 'api2' }
         });
