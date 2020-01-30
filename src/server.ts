2c785313cc05f699475ef6150a1d5eb0e5c186b2
Takayoshi Uchida <takayoshi-uchida@mail.nissan.co.jp>
2020-01-30 10:33:19 +0900

    internal routing test

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,27 @@
 import * as Express from 'express';
 
+require('dotenv').config();
 const app = Express();
 
 app.get(
     '/',
     (req: Express.Request, res: Express.Response) => {
-        return res.send('Hello world.');
+        return res.send('Hello world. api2');
     });
 
+app.get(
+    '/internal-api',
+    (req: Express.Request, res: Express.Response) => {
+        return res.json({
+            data: { from: 'api2' }
+        });
+    });
+
+const port = process.env.PORT || 3000;
 app.listen(
-    3000,
+    port,
     () => {
-        console.log('Example app listening on port 3000!');
+        console.log(`Example app listening on port ${port}!`);
     });
 
 export default app;
